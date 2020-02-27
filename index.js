9cc6801b0acd54a7d246c1e2f581520681780dcd
Leo Picado <leopic@gmail.com>
2020-02-27 16:28:41 -0600

    Bring this down a bit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const slackChannel = core.getInput('slack-channel');
   const jobStatus = core.getInput('job-status');
 
   const payload = github.context.payload;
@@ -14,23 +13,12 @@ try {
 
   const emoji = jobStatus === 'Success' ? ':thumbsup:' : ':thumbsdown:';
 
-  const existing = "" +
-    "*${{github.repository}}*: the result of a ${{github.event_name}} by ${{github.actor}} was: *${{job.status}}*.\n" +
-    "See more <${{github.event.pull_request.html_url}}/checks?check_run_id=${{github.run_id}}|details>." +
-    "";
+  const message = `
+${repoName}: the result of a ${eventName} by ${author} was: ${emoji}
+See more ${prDetails}
+  `;
 
-  const data = {
-    "channel": slackChannel,
-    "text": `
-      ${repoName}: the result of a ${eventName} by ${author} was: ${emoji}\n
-      See more ${prDetails}
-    `
-  };
-
-  const stringifiedData = JSON.stringify(data);
-  const output = stringifiedData.replace(/"/g, '\\"');
-
-  core.setOutput('message', output);
+  core.setOutput('message', message);
 } catch (error) {
   core.setFailed(error.message);
 }
